fde640fa1612cc0a5eb93a304d1c989b31479cce
Avinash Dwarapu <d.nash.avi@gmail.com>
2017-08-03 20:50:20 +0100

    First Commit :tada:

---
+++ lib/lazy_laser.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'lazy_laser/version'
+
+# LazyLazer is a lazy loading model.
+module LazyLaser
+  # Your code goes here...
+end
---
+++ spec/lazy_laser_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe LazyLaser do
+  it 'has a version number' do
+    expect(LazyLaser::VERSION).not_to be nil
+  end
+end
