352c1d3e2055f8de2394c229c9e677fd8597d7fa
Avinash Dwarapu <avinash@dwarapu.me>
2017-10-01 00:37:19 +0100

    Fix issue with store inheritance.

--- lib/lazy_lazer/key_metadata_store.rb
+++ lib/lazy_lazer/key_metadata_store.rb
@@ -11,13 +11,24 @@ module LazyLazer
       @required_properties = []
     end
 
+    # Used for {Object#dup}.
+    def initialize_copy(original)
+      super
+      @collection = original.instance_variable_get(:@collection).dup
+      @required_properties = original.instance_variable_get(:@required_properties).dup
+    end
+
     # Add a KeyMetadata to the store.
     # @param key [Symbol] the key
     # @param meta [KeyMetadata] the key metadata
     # @return [KeyMetadata] the provided meta
     def add(key, meta)
       @collection[key] = meta
-      @required_properties << key if meta.required?
+      if meta.required?
+        @required_properties << key
+      else
+        @required_properties.delete(key)
+      end
       meta
     end
 
